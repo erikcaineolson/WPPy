98bff301a76947a34c0cc9dc16de1329e497586c
Erik C. Olson <erikcaineolson@gmail.com>
2018-09-01 18:00:26 -0700

    Update GitHub URL

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='wppy',
       version='1.0',
       description='Python script for rapidly generating WordPress sites on Linux NGINX servers from the command line',
-      url='https://github.com/erikcaineolson/WPPy',
+      url='https://github.com/erikcaineolson/wppy',
       author='Erik C. Olson',
       author_email='erikcaineolson@gmail.com',
       license='MIT',
